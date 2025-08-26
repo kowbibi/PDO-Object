d994766a6187a2d4a00a7a6a1fbe208d9c1787a0
kowbibi <kristianbernard.cortez.cvt@eac.edu.ph>
2025-08-27 00:44:54 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+class Config {
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'school_db';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+    const DB_CHARSET = 'utf8mb4';
+}
+?>
